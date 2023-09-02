451664076979d3fcceff27bc07c97ef523b79710
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-09-03 02:33:23 +0530

    removed db

--- index.js
+++ index.js
@@ -10,19 +10,19 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
 
 // Connect to MongoDB (replace 'your-database-uri' with your MongoDB connection URI)
-mongoose.connect("mongodb+srv://jagdtri2003:zsL5bsh1uWIqu6Hx@cluster0.tcqpxnn.mongodb.net/Fotogram?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
+// mongoose.connect("mongodb+srv://jagdtri2003:zsL5bsh1uWIqu6Hx@cluster0.tcqpxnn.mongodb.net/Fotogram?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB database');
-});
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB database');
+// });
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
